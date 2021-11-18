f11be7d1c8f979957ab9b428647b3966214349fc
adam19-pl <73747876+adam19-pl@users.noreply.github.com>
2021-11-18 17:43:01 +0100

    Update content in Portfolio

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-92y79nj@%6kls4mn1v2a7t0a)shfdgkupkh91)_1^x17rl5(85'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
